2e1b98e4c8fc8d17b2b6b290c39b976f1825e4e4
alumno <alumno@aula1-01>
2019-04-12 19:51:45 +0200

    falta muchisimo

--- London/src/ioDatos/Datos.java
+++ London/src/ioDatos/Datos.java
@@ -6,14 +6,37 @@
 package ioDatos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author alumno
  */
 public class Datos {
-    File f=new File("archivo.txt");
+
+    public static ArrayList<String> leerarchivo(){
     
+    ArrayList<String> vgent=new ArrayList<>();
+    File f=new File("archivo.txt");
+        
+    try {
+            
+            FileReader fr=new FileReader(f);
+            Scanner leer=new Scanner(fr);
+            while(leer.hasNext()){
+             vgent.add(leer.nextLine());
+            }
+                
+        } catch (FileNotFoundException ex) {
+            System.out.println("no se puede conectar con el fichero");
+        }
+    return vgent;    
+    }
     
       
 }
